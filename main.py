64fdb19b0b7f6b4a7f7b8c4970ac3a7c181ed833
HeeSeung Lee <idghst.dev@gmail.com>
2024-04-13 01:06:51 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import streamlit as st
 import time
-# import pandas as pd
 
-st.write("# ANIMAL GPT")
-# st.write("Hello *wo")
-
-# st.write("st.success('Done!')")
-# st.success('Done!')
+def stream_data(data):
+    for word in data.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
 animal_shelter = ['고양이', '강아지', '토끼', '새']
 
-animal = st.text_input('INPUT')
+st.set_page_config(
+    page_title="DirChat",
+    page_icon=":books:"
+)
 
-with st.spinner('Wait for it...'):
-    # time.sleep(5)
-    if animal:
-        have_it = animal.lower() in animal_shelter
-        '그거 있음' if have_it else '그건 없음!!!'
+st.title(":red[Animal] Chat")
+
+if "conversation" not in st.session_state:
+    st.session_state.conversation = None
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = None
+
+if "processComplete" not in st.session_state:
+    st.session_state.processComplete = None
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [{"role": "assistant", 
+                                    "content": "안녕하세요! 동물을 맞춰보세요!"}]
+# if 'messages' not in st.session_state:
+#     st.session_state['messages'] = [{"role": "assistant", 
+#                                     "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if query := st.chat_input("질문을 입력해주세요."):
+    st.session_state.messages.append({"role": "user", "content": query})
+    
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    with st.chat_message("assistant"):
+        have_it = query.lower() in animal_shelter
+        response = '정답!!' if have_it else '오답!!!!'
+        st.write_stream(stream_data(response))
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
